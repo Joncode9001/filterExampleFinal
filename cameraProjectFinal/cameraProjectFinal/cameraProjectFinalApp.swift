6464cb27bb34bbb156631e655b9956f31aeffdc9
Joncode9001 <jaytuby@gmail.com>
2023-03-17 09:44:46 -0700

    made like 1 change

---
+++ cameraProjectFinal/cameraProjectFinal/cameraProjectFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraProjectFinalApp.swift
+//  cameraProjectFinal
+//
+//  Created by Jonah Reinis on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct cameraProjectFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
